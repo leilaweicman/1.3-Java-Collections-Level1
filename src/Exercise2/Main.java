37e9cbc6924fe0218d38006f66884ad619156417
leilaweicman <leilaweic@hotmail.com>
2025-09-16 11:12:35 +0200

    feat: create and initialize integer list

---
+++ src/Exercise2/Main.java
@@ -0,0 +1,16 @@
+package Exercise2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        //Create Integer List
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+        integerList.add(2);
+        integerList.add(3);
+        integerList.add(4);
+        integerList.add(5);
+    }
+}
