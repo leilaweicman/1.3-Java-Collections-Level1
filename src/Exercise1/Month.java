fe1f6e678e5977df0a47bf8b25a8e4e0abe9cbbc
leilaweicman <leilaweic@hotmail.com>
2025-09-16 10:13:28 +0200

    feat: add Month class with name field, constructor and getter

---
+++ src/Exercise1/Month.java
@@ -0,0 +1,14 @@
+package Exercise1;
+
+public class Month {
+
+    private String name;
+
+    public Month (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
